d4cd374d6aad44390b5bac153165d1107fb857f9
Audouard Florian <nairolf.audouard@gmail.com>
2025-05-14 11:36:44 +0200

    ajout d'une route pour initialise l'algorithme

--- server.py
+++ server.py
@@ -1,23 +1,43 @@
-from fastapi import FastAPI, Query, File, UploadFile
-
+from fastapi import FastAPI, Query, File, UploadFile, Request
+import pandas as pd
 
 from recomendation.svd.svd_recommender import SVDRecommender
 from recomendation.preprocessing.movie_manipulation import load_data
+from io import StringIO
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
 folder = "0.1"
-FORCE_TRAINING = True
+FORCE_TRAINING = False
 PRODUCTION = True
 
 ALGORITHM = SVDRecommender
-algo = SVDRecommender(FORCE_TRAINING, folder)
+algo = None
+algo = ALGORITHM(FORCE_TRAINING, folder)
 algo.init_data(load_data(f"ml-{folder}m"))
 algo.load()
 
 
-def read_root():
-    return "Welcome to the Movie Recommender API!"
+class AlgorithmNotInitialized(Exception):
+    def __init__(self):
+        self.name = "Algorithm not initialized"
+
+
+@app.exception_handler(AlgorithmNotInitialized)
+async def my_custom_exception_handler(request: Request, exc: AlgorithmNotInitialized):
+    return JSONResponse(
+        status_code=418,
+        content={"message": f"{exc.name}"},
+    )
+
+
+def verify_algo():
+    """
+    Verify if the algorithm is initialized
+    """
+    if algo is None:
+        raise AlgorithmNotInitialized()
 
 
 @app.get("/recommendations/{user_id}")
@@ -30,6 +50,7 @@ def get_recommendations(user_id: int, top_n: int = Query(..., gt=0)):
     Returns:
         List of recommended items
     """
+    verify_algo()
     print(f"Getting recommendations for user {user_id} with top_n={top_n}")
     tmp = algo.get_recommendations(user_id=user_id, top_n=top_n)
     print(tmp)
@@ -39,6 +60,12 @@ def get_recommendations(user_id: int, top_n: int = Query(..., gt=0)):
 @app.post("/upload/")
 async def upload_file(file: UploadFile = File(...)):
     contents = await file.read()
+    df_ratings = None
+    try:
+        df_ratings = pd.read_csv(StringIO(contents.decode("utf-8")))
+    except Exception as e:
+        return {"error": f"Could not parse uploaded file: {e}"}
+    global algo
     algo = ALGORITHM(FORCE_TRAINING, folder)
-    algo.init_data(contents)
+    algo.init_data(df_ratings)
     algo.load()
