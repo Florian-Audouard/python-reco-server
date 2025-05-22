6bcf5ba9d529517303d7b82686829917100b2053
Hermelin_Loïs <hermelin.lois06@gmail.com>
2025-05-22 10:38:07 +0200

    update méthodes, requirement et serveur

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI, Query, File, UploadFile
 
 from recomendation.cold_recommendation.dbscan_recommender import DBSCANRecommender
 from recomendation.svd.svd_recommender import SVDRecommender
-from recomendation.preprocessing.movie_manipulation import load_data_from_url
+from recomendation.preprocessing.movie_manipulation import load_data_from_file
 
 app = FastAPI()
 
@@ -11,7 +11,7 @@ app = FastAPI()
 FORCE_TRAINING = False
 PRODUCTION = True
 
-ratings, movies = load_data_from_url()
+ratings, movies = load_data_from_file("ml-01-m")
 
 algo = SVDRecommender(PRODUCTION, FORCE_TRAINING)
 algo.init_data(ratings, movies)
