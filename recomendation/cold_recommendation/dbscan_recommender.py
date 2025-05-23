45164d55bd564c0a88f573b97dec628dcc9419fa
Hermelin_Loïs <hermelin.lois06@gmail.com>
2025-05-23 09:45:22 +0200

    update dbscan recommendation

--- recomendation/cold_recommendation/dbscan_recommender.py
+++ recomendation/cold_recommendation/dbscan_recommender.py
@@ -3,8 +3,10 @@ import sys
 import pickle
 import random as rd
 import copy
-
-from .dbscan_generation import dbscan_clustering
+import pandas as pd
+import math
+from dbscan_generation import dbscan_clustering
+from concurrent.futures import ThreadPoolExecutor
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from model import Model
@@ -13,30 +15,60 @@ from preprocessing.movie_manipulation import load_data_from_file
 FORCE_TRAINING = True
 FOLDER_SET = "0.1"
 
-class DBSCANRecommender(Model):
+class DBSCANRecommender(Model):    
+
     def __init__(self, production=False, force_training=False):
         super().__init__(production, force_training)
         self.model = None
-        self.filename = "dbscan_model.pkl"
+        self.filename = "dbscan_model_and_data.pkl"
         self.movieId_to_idx = None
         self.idx_to_movieId = None
 
+        self.limit_watch = 0
+        self.limit_rate = 3
+        self.recommendable_movies = None
+
     def training_impl(self):
-        label_and_movies_data = dbscan_clustering(self.ratings, self.movies)
-        for label, movies_data in label_and_movies_data.items():
-            label_and_movies_data[label] = sorted(movies_data, key=lambda x: (x[1], -x[2]), reverse=True)
+        recommendable_movies = set()
+        for movie_id in self.movies["movieId"]:
+            number_of_watch = self.ratings[self.ratings["movieId"] == movie_id]["rating"].count()
+            average_rate = self.ratings[self.ratings["movieId"] == movie_id]["rating"].mean()
+            if number_of_watch <= self.limit_watch or average_rate >= self.limit_rate:
+                recommendable_movies.add(movie_id)
+        self.recommendable_movies = recommendable_movies
+        label_and_movies_data = dbscan_clustering(self.ratings, self.movies, self.recommendable_movies)
+
+        def sort_cluster(cluster_items):
+            key = cluster_items[0]
+            cluster = cluster_items[1]
+            return (key, sorted(cluster, key=lambda x: (x[1], -x[2]), reverse=True))
+        
+        with ThreadPoolExecutor(max_workers=10) as executor:
+            results = executor.map(
+                sort_cluster,
+                label_and_movies_data.items()
+                )
+
+        for key, sorted_cluster in results:
+            label_and_movies_data[key] = sorted_cluster
         self.model = label_and_movies_data
 
     def save(self):
         if self.model is None:
             raise RuntimeError(self.ERROR_MESSAGE)
         with open(self.get_full_path(), "wb") as f:
-            pickle.dump(self.model, f)
+            pickle.dump({
+                "model": self.model,
+                "recommendable_movies": self.recommendable_movies
+            }, f)
+
 
     def load_impl(self):
         os.makedirs(self.data_dir, exist_ok=True)
         with open(self.get_full_path(), "rb") as f:
-            self.model = pickle.load(f)
+            data = pickle.load(f)
+            self.model = data["model"]
+            self.recommendable_movies = data["recommendable_movies"]
 
     def get_select_random_movie(self, model_memory):
         """
@@ -53,63 +85,117 @@ class DBSCANRecommender(Model):
                 - selected_movie: the selected (movieId, mean, number_of_watch) tuple or None
                 - index_to_remove: index of the selected movie in selected_list or None
         """
+
         label = rd.choice(list(model_memory.keys()))
         selected_list = model_memory[label]
 
-        index_to_remove = None
-
-        if selected_list[0][1] > 2.5 and selected_list[-1][2] == 0:
-            if rd.random() < 0.8:
-                selected_movie = selected_list[0]
-                index_to_remove = 0
-            else:
-                selected_movie = selected_list[-1]
-                index_to_remove = -1
-        elif selected_list[0][1] > 2.5:
-            selected_movie = selected_list[0]
-            index_to_remove = 0
-        elif selected_list[-1][2] == 0:
-            selected_movie = selected_list[-1]
-            index_to_remove = -1
-        else:
-            selected_movie = None
-            index_to_remove = None
-        return label, selected_list, selected_movie, index_to_remove
+        if len(selected_list) == 0:
+            return label, None, None, None
+        else : 
+            def generate_utility_value(movie):
+                average_rate = movie[1]
+                return (movie[0], math.exp(-0.*average_rate**2))
             
+            total_value = 0
+            probabilities = []
+
+            rd.shuffle(selected_list)
+            for index, movie in enumerate(selected_list):
+                movie_id, utility_value = generate_utility_value(movie)
+                total_value += utility_value
+                if index == 0:
+                    probabilities.append((movie_id, utility_value))
+                else:
+                    probabilities.append((movie_id, utility_value + probabilities[index - 1][1]))
+            
+            random_value = rd.uniform(0, total_value)
+            for index in range(len(probabilities)):
+                if random_value <= probabilities[index][1]:
+                    return label, selected_list, probabilities[index][0], index
 
     def get_recommendations(self, user_id , top_n):
         model_memory = copy.deepcopy(self.model)
         selected_movies_id = []
+        all_movies_id = self.ratings["movieId"].unique()
+        if top_n > len(all_movies_id):
+            return all_movies_id
         while len(selected_movies_id) < top_n and len(model_memory) > 0:
-            if not model_memory:
+            if len(model_memory) == 0:
                 break 
             label, selected_list, selected_movie, index_to_remove = self.get_select_random_movie(model_memory)
-            if selected_movie is None:
+            if selected_list is None:
                 del model_memory[label]
-
-            selected_movies_id.append(selected_movie[0])
-            del selected_list[index_to_remove]
+                continue
+            selected_movies_id.append(selected_movie)
+            selected_list.pop(index_to_remove)
         return selected_movies_id
-
+    
     def init_data_impl(self):
-        pass
+        return 
+    
+    def predict(self, user_id, candidates):
+        return 
+    
+    def get_diversity(self, recommendations):
+        genres_list_recommended = set()
+        not_watch_movies = set()
+        for movie_id in recommendations:
+            genres = self.movies[self.movies["movieId"] == movie_id]["genre"].values
+            number_of_watch = self.ratings[self.ratings["movieId"] == movie_id]["rating"].count()
+            if number_of_watch <= self.limit_watch:
+                not_watch_movies.add(movie_id)
+            if len(genres) != 0:
+                genres_list_recommended.update(genres[0].split("|"))
+        genre_diversity = len(set(genres_list_recommended))
+        unknown_recommended_movies = len(not_watch_movies)
+        return genre_diversity, unknown_recommended_movies
+    
 
-    def get_prediction_set(self):
-        pass
+    def accuracy(self, k=30):
+        if self.validation_set is None:
+            raise RuntimeError("Validation set not initialized")
+        
+        different_genre_in_db = set()
+        different_genre_in_recommendation = set()
+        different_unknown_movies = set()
 
-    def predict(self, user_id, list_movie_id):
-        pass
+        for movie_id in self.movies["movieId"]:
+            genres = self.movies[self.movies["movieId"] == movie_id]["genre"].values
+            number_of_watch = self.ratings[self.ratings["movieId"] == movie_id]["rating"].count()
+            if len(genres) != 0:
+                different_genre_in_db.update(genres[0].split("|"))
+            if number_of_watch <= self.limit_watch:
+                different_unknown_movies.add(movie_id)
+            if movie_id in self.recommendable_movies and len(genres) != 0:
+                    different_genre_in_recommendation.update(genres[0].split("|"))
 
-    def accuracy(self, k=10):
+        number_of_genre_in_db = len(different_genre_in_db)
+        number_of_genre_in_recommendation = len(different_genre_in_recommendation)
+        number_of_unknown_movies = len(different_unknown_movies)
+        number_of_movies = len(self.recommendable_movies)
+
+
+        recommended_diversity = set()
+        genre_diversity = []
+        unknown_diversity = []
+
+        for user_id in self.validation_set["userId"].unique():
+            
+            recommendations = self.get_recommendations(user_id, k)
+            genre_diversity_recommended, unknown_recommended_movies = self.get_diversity(recommendations)
+
+            recommended_diversity.update(recommendations)
+            genre_diversity.append(genre_diversity_recommended)
+            unknown_diversity.append(unknown_recommended_movies)
+
         return {
-            "accuracy": 0.0,
-            "precision": 0.0,
-            "recall": 0.0,
-            "f1_score": 0.0,
+            "mean genre diversity": sum(genre_diversity) / (len(genre_diversity)*number_of_genre_in_db),
+            "mean genre inner diversity": sum(genre_diversity) / (len(genre_diversity)*number_of_genre_in_recommendation),
+            "mean unknown selection": sum(unknown_diversity) / (len(unknown_diversity)*number_of_unknown_movies),
+            "coverage": len(recommended_diversity) / number_of_movies
         }
 
 if __name__ == "__main__":
-    recommender = DBSCANRecommender(False,True)
+    recommender = DBSCANRecommender(False, True)
     ratings, movies = load_data_from_file(f"ml-{FOLDER_SET}m")
     recommender.testing_main(ratings, movies)
-
